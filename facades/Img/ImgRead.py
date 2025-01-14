3c1fa77a6e4c093adde88edf9cc18cd933f5b66a
free-abrams <40207901+free-abrams@users.noreply.github.com>
2025-01-14 17:29:39 +0800

    fear: 图片模板池

---
+++ facades/Img/ImgRead.py
@@ -0,0 +1,24 @@
+import pathlib
+
+import cv2
+
+from facades.Constant.Constant import IMG_PATH
+
+IMG_POOL = {}
+
+def MyImread(path:pathlib.PurePath|str):
+    p = f"f{path}"
+
+    if p in IMG_POOL:
+        return IMG_POOL[p]
+    else:
+        IMG_POOL[p] = cv2.imread(p)
+        return  IMG_POOL[p]
+
+
+if __name__ == "__main__":
+    img = IMG_PATH.joinpath("WorldTree").joinpath("quit.png")
+    MyImread(img)
+    print(f"{len(IMG_POOL)}\n")
+    MyImread(img)
+    print(f"{len(IMG_POOL)}\n")
