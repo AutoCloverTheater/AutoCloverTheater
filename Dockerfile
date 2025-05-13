7f80ba9d1f6d0f31ad7633be8f683197dfd84637
听说卡普空不炒冷饭 <sa130494625@qq.com>
2025-05-13 09:26:59 +0800

    refactor: 重构 Docker部署并更新依赖
    - 新增 .dockerignore 文件以忽略不必要的文件和目录
    - 更新 Dockerfile 基础镜像到 python:3.13-slim- 添加升级 pip 的步骤
    - 精简 requirements.txt 文件中的依赖
    - 移除 PyQt5 和 PyQtWebEngine 依赖
    - 更新 numpy 依赖到 2.2.5 版本
    - 修正 Flask 和 flask-cors依赖版本

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # 使用官方 Python 镜像
-FROM python:3.10-slim
+FROM python:3.13-slim
 
 # 设置工作目录
 WORKDIR /app
@@ -7,6 +7,7 @@ WORKDIR /app
 # 复制当前目录内容到容器中的 /app 目录
 COPY . /app
 
+RUN pip install --upgrade pip
 # 安装依赖
 RUN pip install --no-cache-dir -r requirements.txt
 
--- main.py
+++ main.py
@@ -8,71 +8,16 @@ import shutil
 import zipfile
 
 from src.app import app
-
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtWidgets import QApplication, QDesktopWidget, QSizePolicy
-
-from src.facades.Constant.Constant import IMG_PATH, APP_PATH
-from src.facades.Logx.Logx import logx
 from pathlib import Path
 
 logging.getLogger("flask").setLevel(logging.ERROR)
 logging.getLogger("werkzeug").setLevel(logging.ERROR)
 
 port = 8233
-
-def mainWindow(port = 8233):
-    if sys.platform == "win32":
-        myappid = "github.clover.auto3"  # 替换为你的唯一标识符
-        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
-
-    file_path = Path(APP_PATH.joinpath("webui/index.html"))  # 替换为你的文件路径
-
-    content = file_path.read_text(encoding="utf-8").replace("http://localhost:8233", f"http://localhost:{port}")  # 读取文本文件
-
-    window = (1190,720)
-
-    # 启动应用
-    app = QApplication([])
-    app.setApplicationName("四叶草🍀小助手")
-    icon = QIcon(f'{IMG_PATH.joinpath("uiWindowIcon.jpg")}')
-    app.setStyle("Fusion")
-
-    app.setWindowIcon(icon)
-
-    # # 获取屏幕对象
-    screen = QDesktopWidget().screenGeometry()
-
-    # # 获取屏幕宽高
-    screen_width = screen.width()  # 屏幕宽度
-    screen_height = screen.height()  # 屏幕高度
-    left_point = (
-        int(screen_width/2) - int(window[0]/2),
-        int(screen_height/2)- int(window[1]/2),
-    )
-
-    # 创建浏览器窗口
-    browser = QWebEngineView()
-    browser.setHtml(content)  # 渲染 HTML 内容
-    # 设置窗口位置和大小（x, y, width, height）
-    # 设置大小策略（关键）
-    browser.setSizePolicy(
-        QSizePolicy.Expanding,  # 水平策略
-        QSizePolicy.Expanding  # 垂直策略
-    )
-    browser.setMinimumSize(window[0], window[1])
-    browser.setGeometry(left_point[0], left_point[1], window[0], window[1])  # 窗口左上角
-    browser.setWindowIcon(icon)
-    browser.setAcceptDrops(False)
-    browser.show()
-
-
-    # 运行应用
-    app.exec_()
-
-threading.Thread(target=app.run, kwargs={'port': port, 'threaded': True}, daemon=True).start()
-logx.info(f"listing port at {port}")
+if __name__ == '__main__':
+    app.run(port=port, threaded=True)
+# threading.Thread(target=app.run, kwargs={'port': port, 'threaded': True}, daemon=True).start()
+# logx.info(f"listing port at {port}")
 # while True:
 #     logx.info(f"listing port at {port}")
 #     logx.debug(f"listing port at {port}")
@@ -151,5 +96,3 @@ def extract_archive_file(archive_file, file, destination_folder):
     if extension == 'zip':
         with zipfile.ZipFile(archive_file, 'r') as archive:
             archive.extract(member=file, path=destination_folder)
-
-mainWindow(port)
