e0dd7513c88d845f49233e1b7fe1f508687f88e5
听说卡普空不炒冷饭 <sa130494625@qq.com>
2025-05-29 09:44:52 +0800

    codestyle(relic):编码风格

--- test/relic.py
+++ test/relic.py
@@ -1,20 +1,16 @@
 # 遗迹探索-【噩梦难度】【热砂】
-import threading
-import time
-
 from act.facades.Emulator.Emulator import ConnectEmulator
 from act.facades.Runner.RelicRunner import beforeRelic, inRelic
 from act.facades.Runner.layout.AdventureRunner import FindAdventure
 from act.facades.Runner.layout.LoginRunner import Login
 
-
 def run():
-        ConnectEmulator()
-        Login()
-        FindAdventure("hasRelicButton")
-        beforeRelic()
-        inRelic()
+	ConnectEmulator()
+	Login()
+	FindAdventure("hasRelicButton")
+	beforeRelic()
+	inRelic()
 
 if __name__ == '__main__':
-    for i in range(4):
-            run()
+	for i in range(4):
+		run()
